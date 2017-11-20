dbcc8718e732070b546d901a5a7dc6bd966f3969
Avelino <t@avelino.xxx>
2017-11-20 02:28:09 -0200

    rename repl to interative

---
+++ interative/interative_test.go
@@ -0,0 +1,25 @@
+package repl
+
+import (
+	"os/user"
+	"testing"
+)
+
+func TestExpandFilename(t *testing.T) {
+	data := map[string]string{
+		"/home/plo~p": "/home/plo~p",
+		"~/plop":      "/foo/plop",
+		// Unsupported expansion
+		"~plop": "~plop",
+	}
+
+	for input, expected := range data {
+		s, err := ExpandFilename(input, &user.User{HomeDir: "/foo"})
+		if err != nil {
+			t.Errorf("Unpexpected error: %v", err)
+		}
+		if s != expected {
+			t.Errorf("Input %q - expected %q, got %q", input, expected, s)
+		}
+	}
+}
